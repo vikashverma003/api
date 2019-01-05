63fa593497cbe40b509aef5a1fe0e89815e4c6a6
vikashverma003 <vikashverma003@gmail.com>
2019-01-06 00:02:16 +0530

    Relationship created

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -8,9 +8,13 @@ $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
 
     'name'=>$faker->word,
+
     'detail'=>$faker->paragraph,
+    
     'price'=>$faker->numberBetween(100, 1000),
+    
     'stock'=>$faker->randomDigit,
+    
     'discount'=>$faker->numberBetween(2, 30)
     
     
