225188eebc315b88bf3214ba7ee0e17817ef8f53
vikashverma003 <vikashverma003@gmail.com>
2019-01-06 19:14:27 +0530

    modified product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -16,15 +16,27 @@ class ProductResource extends Resource
     {
         
         return[
+
         'name'=>$this->name,
 
         'description'=>$this->detail,
 
         'price'=>$this->price,
 
-        'stock'=>$this->stock,
+        'stock'=>$this->stock ==0? 'not stock': $this->stock,
 
-        'discount'=>$this->discount
+        'discount'=>$this->discount,
+
+        'totalPrice'=>round((1- ($this->discount/100))*$this->price, 2),
+
+
+        'rating'=>$this->reviews->count()>0 ? round($this->reviews->sum('star')/$this->reviews->count(), 2):'no rating yet',
+
+        'href'=>[
+
+        'reviews'=>route('reviews.index', $this->id)
+
+        ]
 
         ];
 
